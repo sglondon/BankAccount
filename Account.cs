661ed5265deafc3f84506469be0874e7a05a67b1
sglondon <susangs2@gmail.com>
2018-02-07 13:32:56 -0500

    first commit

---
+++ Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    public abstract class Account
+    {
+        //abstract method
+
+        public abstract void DisplayBalance();
+
+
+
+    }
+}
---
+++ Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    public class Client : Account
+    {
+        //fields
+        //invisible
+
+        //Properties
+
+        public string AcctNum { get; set; }
+        public string AcctType { get; set; }
+        public double Balance { get; set; }
+
+        //Constructors
+
+        public Client()
+        {
+            //default constructor
+        }
+
+        public Client(string AcctNum, string AcctType, double Balance)
+        {
+        AcctNum = acctNum;
+        AcctType = acctType;
+        Balance = balance;
+
+        }
+        public override void AccountNum()
+        {
+            Console.WriteLine("account number");
+        }
+    }
+
+
+
+}
